044058ad4691170f4280710584fc483f423947fd
Kiran Bandela <125917383+NexaNomad@users.noreply.github.com>
2025-01-09 18:56:48 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,8 @@
+"""This is a simple Python program that prints 'Hello, World!'."""
+
+def main():
+    """Prints 'Hello, World!' to the console."""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
